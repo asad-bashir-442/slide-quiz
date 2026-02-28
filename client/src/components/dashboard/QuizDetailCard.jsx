713b0bdf01441ed68a4a88c328575fba893bfc15
Kirsten <ka147@myscc.ca>
2026-02-28 16:21:18 -0500

    Quiz Detail Card -Created detail card -layout needs tuning

---
+++ client/src/components/dashboard/QuizDetailCard.jsx
@@ -0,0 +1,21 @@
+export function QuizDetailCard({quizName, description, dateCreated, numQuestions}){
+    return (
+        <div className="card w-96 bg-base-100 card-md shadow-sm">
+            <div className="card-body">
+                <h2 className="card-title">{quizName}</h2>
+                <p>{description}</p>
+                <div class="badges">
+                    <div className="badge badge-ghost">Created on: {dateCreated}</div>
+                    <div className="badge badge-ghost">{numQuestions} questions</div>
+                </div>
+
+                <div className="justify-end card-actions">
+                    <button className="btn btn-primary">Edit Quiz</button>
+                    <button className="btn btn-secondary">Download</button>
+                    <button className="btn btn-accent">Host</button>
+                    <button className="btn btn-error">Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+}
