385906fdc16f51d9499e5a9612dd7855e4fb18b5
Kirsten <ka147@myscc.ca>
2026-02-28 18:21:44 -0500

    Removed unneeded text from dash page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import {QuizDetailCard} from "../components/dashboard/QuizDetailCard.jsx";
 export function Dashboard() {
     return (
         <div className="flex flex-col">
-        Dashboard Page
+
 
      <DashWelcomePanel username={"User"}/>
         {/*<QuizDetailCard quizName="Example" description="lorem ipsum "*/}
