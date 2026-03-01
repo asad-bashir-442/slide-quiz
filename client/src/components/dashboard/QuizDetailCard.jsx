d128f8b6099ca4eb28f10cee51424ea1b9120201
Kirsten <ka147@myscc.ca>
2026-03-01 17:24:28 -0500

    Quiz Detail Card - fixed import error - fixed card layout - needs a grid to list all quizzes in component once API/Database are working

--- client/src/components/dashboard/QuizDetailCard.jsx
+++ client/src/components/dashboard/QuizDetailCard.jsx
@@ -1,21 +1,27 @@
-export function QuizDetailCard({quizName, description, dateCreated, numQuestions}){
+export function QuizDetailCard({ quizName, description, dateCreated, numQuestions }) {
     return (
-        <div className="card w-96 bg-base-100 card-md shadow-sm">
+        <div className="card w-96 m-10 bg-base-100 shadow-sm">
             <div className="card-body">
                 <h2 className="card-title">{quizName}</h2>
-                <p>{description}<br/>
-                s
-                    <div className="badge badge-ghost">Created on: {dateCreated}</div>
-                    <div className="badge badge-ghost">{numQuestions} questions</div>
-                </p>
 
-                <div className="justify-end card-actions">
-                    <button className="btn btn-primary">Edit Quiz</button>
+                <p>{description}</p>
+
+                <div className="flex gap-2 mt-2">
+                    <div className="badge badge-ghost">
+                        Created on: {dateCreated.toLocaleDateString()}
+                    </div>
+                    <div className="badge badge-ghost">
+                        {numQuestions} questions
+                    </div>
+                </div>
+
+                <div className="card-actions justify-center flex-nowrap gap-3 mt-4">
+                    <button className="btn btn-primary">Edit</button>
                     <button className="btn btn-secondary">Download</button>
                     <button className="btn btn-accent">Host</button>
                     <button className="btn btn-error">Delete</button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import {DashWelcomePanel} from "../components/dashboard/DashWelcomePanel.jsx";
+import {QuizDetailCard} from "../components/dashboard/QuizDetailCard.jsx";
 
 
 export function Dashboard() {
@@ -7,8 +8,8 @@ export function Dashboard() {
 
 
      <DashWelcomePanel username={"User"}/>
-        {/*<QuizDetailCard quizName="Example" description="lorem ipsum "*/}
-        {/*                dateCreated={new Date()} numQuestions="13" />*/}
+        <QuizDetailCard quizName="Example" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum eget ante a fermentum. Nulla facilisi. Quisque rhoncus gravida leo, eu tincidunt elit vestibulum et. Phasellus orci eros, varius vitae sem luctus, eleifend pulvinar mi"
+                        dateCreated={new Date()} numQuestions="13" />
     </div>
     )
 }
