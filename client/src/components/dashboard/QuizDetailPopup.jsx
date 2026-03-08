cabdc2a32e21ebe23ab0da7106569c4b7da2cd96
Kirsten <ka147@myscc.ca>
2026-03-08 18:25:30 -0400

    Quiz Details Popup - added base for modal form

--- client/src/components/dashboard/QuizDetailPopup.jsx
+++ client/src/components/dashboard/QuizDetailPopup.jsx
@@ -1,4 +1,4 @@
-export function QuizDetailPage() {
+export function QuizDetailPopup({ quizName, description, dateCreated, numQuestions }) {
     return (
        <div className="flex flex-1 flex-col"></div>
     )
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,13 @@
 import {DashWelcomePanel} from "../components/dashboard/DashWelcomePanel.jsx";
 import {QuizDetailCard} from "../components/dashboard/QuizDetailCard.jsx";
-
+import {QuizDetailPopup} from "../components/dashboard/QuizDetailPopup.jsx";
 
 export function Dashboard() {
     return (
         <div className="flex flex-col">
 
-
+    <QuizDetailPopup quizName="Example" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum eget ante a fermentum. Nulla facilisi. Quisque rhoncus gravida leo, eu tincidunt elit vestibulum et. Phasellus orci eros, varius vitae sem luctus, eleifend pulvinar mi"
+                     dateCreated={new Date()} numQuestions="13"/>
      <DashWelcomePanel username={"User"}/>
         <QuizDetailCard quizName="Example" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum eget ante a fermentum. Nulla facilisi. Quisque rhoncus gravida leo, eu tincidunt elit vestibulum et. Phasellus orci eros, varius vitae sem luctus, eleifend pulvinar mi"
                         dateCreated={new Date()} numQuestions="13" />
