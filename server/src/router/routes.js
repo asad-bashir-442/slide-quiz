33e6209821cc6fb4c4bbcf8b31bda1dc80e8e898
mseip <w0864556@myscc.ca>
2026-03-09 05:08:35 -0400

    feat: Quiz API CRUD

--- server/src/router/routes.js
+++ server/src/router/routes.js
@@ -4,6 +4,12 @@ import auth from "../middleware/auth.js";
 import allow from "../middleware/allow.js";
 
 import { register, login, whoami, update } from "../handlers/auth.js";
+import {
+    createQuiz,
+    getQuiz,
+    updateQuiz,
+    deleteQuiz,
+} from "../handlers/quiz.js";
 
 // Public routes
 router.register(async (r) => {
@@ -21,4 +27,10 @@ router.register(async (r) => {
     // Auth
     r.post("/@me", { onRequest: [r.auth] }, whoami);
     r.patch("/@me", { onRequest: [r.auth] }, update);
+
+    // Quiz
+    r.post("/@me/quiz", { onRequest: [r.auth] }, createQuiz);
+    r.get("/@me/quiz/:id", { onRequest: [r.auth] }, getQuiz);
+    r.patch("/@me/quiz/:id", { onRequest: [r.auth] }, updateQuiz);
+    r.delete("/@me/quiz/:id", { onRequest: [r.auth] }, deleteQuiz);
 });
