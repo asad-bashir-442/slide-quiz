05e2c3fbf0acaf5b8c64883a5d2dff4a98760ab1
Kirsten <ka147@myscc.ca>
2026-03-28 15:24:52 -0400

    API code is there as best I could figure out without being able to test it. I just commented it out for now.

---
+++ client/src/components/dashboard/results/session/SessionResultsPanel.jsx
@@ -0,0 +1,43 @@
+// import {useEffect, useState} from "react";
+// import {useParams} from "react-router";
+// import {getQuizById} from "../../../../api/quiz.js";
+// import {getAllQuestionsById} from "../../../../api/editor.js";
+
+export function SessionResultsPanel() {
+    // const [session, setSession] = useState(null);
+    // const [severError, setServerError] = useState(null);
+    // let { id } = useParams();
+    //
+    // useEffect(() => {
+    //     const fetchResults = async () => {
+    //         try {
+    //             const sessionData = await getResultsById(id);
+    //             const fullQuizData = {
+    //                 ...sessionData.data,
+    //                 ...sessionData.data,
+    //             };
+    //
+    //             setSession(fullQuizData);
+    //
+    //             console.dir(fullQuizData);
+    //         } catch (error) {
+    //             setServerError(error.message);
+    //         }
+    //     };
+    //
+    //     fetchResults();
+    // }, [id]);
+
+    // if (severError) {
+    //     return (
+    //         <div className="flex flex-1 flex-col justify-center items-center">
+    //             <h1 className="text-6xl">404</h1>
+    //             <p>Quiz Not Found</p>
+    //         </div>
+    //     );
+    // }
+
+    return(
+        <div></div>
+    );
+}
