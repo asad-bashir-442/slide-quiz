53ff4806ddb6450a63e7996f8695cb468cd0bbe0
Asad Bashir <ab442@myscc.ca>
2026-03-31 03:35:58 -0400

    configured client side routes for responses

--- client/src/api/responses.js
+++ client/src/api/responses.js
@@ -18,3 +18,56 @@ export async function getAllResponses() {
 
   return data;
 }
+
+export async function getReponseById(id) {
+  const url = `${BASE_URL}/@me/response/${id}`;
+  const token = localStorage.getItem("token");
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    return Error(data.message || "Something went wrong");
+  }
+  return data;
+}
+
+export async function deleteAllResponse() {
+  const url = `${BASE_URL}/@me/response`;
+  const token = localStorage.getItem("token");
+  const res = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    return Error(data.message || "Something went wrong");
+  }
+  return data;
+}
+
+export async function deleteResponseById(id) {
+  const url = `${BASE_URL}/@me/response/${id}`;
+  const token = localStorage.getItem("token");
+  const res = fetch(url, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!res.ok) {
+    return Error(data.message || "Something went wrong");
+  }
+
+  return data;
+}
